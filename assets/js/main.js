b9b85d7ba974cd623b0d466197296b30f8242b46
eunsunp <espak07@gmail.com>
2021-12-01 12:53:12 +0900

    edit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    // autoplay: {
+    // delay: 2500,
+    // disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    grabCursor: true,
+    effect: 'fade',
+  });
+
+  var swiper = new Swiper(".mySwiper-02", {
+    
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+  });
+
+  
