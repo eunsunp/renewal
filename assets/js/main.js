94fc5c557902e550cc9d4dc986bff9fc411d9d44
eunsunp <espak07@gmail.com>
2021-12-02 11:48:26 +0900

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    },
+    // autoplay: {
+    // delay: 2500,
+    // disableOnInteraction: false,
+    // },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
